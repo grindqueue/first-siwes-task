230de23109573ab98e6a0a58c14d7f29878caddb
Ope <omolewa.opeyemi@lmu.edu.ng>
2025-04-30 07:14:52 +0100

    ADD URL encoded to allow allow backend read html forms

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("<h1>You are connected to our server<h1/>");
 });
