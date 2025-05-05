3f105012e0bcea43c749fd2510102da17cd88454
Ope <omolewa.opeyemi@lmu.edu.ng>
2025-05-05 09:57:19 +0100

    Update the formsId schema

--- models/formsId.js
+++ models/formsId.js
@@ -6,15 +6,21 @@ const formsIdSchema = new Schema({
   Username : {
     type:String,
     required: [true,'Please enter your username'],
+    minlength : [6, "User name can not be less than six characters"],
+    maxLength : [100,"Username should not exceed 100 characters"],
   },
   Email:{
     type:String,
     required:[true,"Please enter your email address"],
-    unique: [true, "Email already used to register"]
+    unique: [true, "Email already used to register"],
+    match: [/.+@.+\..+/, "Please enter a valid email address"],
+    trim : true,
+    lowercase: true,
   },
   Phone_number:{
     type:String,
-    required:[true,"Kindly enter your phone number"]
+    required:[true,"Kindly enter your phone number"],
+    match: [/^\+?\d{10,15}$/, "Please enter a valid phone number"]
   }
 },{
   timestamps : true,
